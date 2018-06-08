d5a80daa99c4cca17a0441971da8a1e75cb26c5f
CircleCI <shadowimmage@gmail.com>
2018-06-08 23:23:40 +0000

    CircleCI site rebuild at Fri Jun  8 23:23:40 UTC 2018 [skip ci]

--- js/search.js
+++ js/search.js
@@ -13,7 +13,7 @@ var fuseOptions = {
   keys: [
     {name:"title",weight:0.8},
     {name:"contents",weight:0.5},
-    {name:"tags",weight:0.3},
+    {name:"tags",weight:0.4},
     {name:"categories",weight:0.3}
   ]
 };
